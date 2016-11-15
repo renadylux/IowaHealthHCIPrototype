5a73012541cebd7a16a07f7aaf2a5c997a26f192
Ryan Wedoff <Ryan Wedoff>
2016-11-14 18:03:00 -0600

    Added journals

---
+++ JS/SaveJournal.js
@@ -0,0 +1,24 @@
+
+ $( document ).ready(function(){
+     $("#SaveJournal").click(function resetJournal(){
+        var jText = $("#journalArea").val();
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+        if(dd<10) {
+            dd='0'+dd
+        } 
+
+        if(mm<10) {
+            mm='0'+mm
+        } 
+
+        today = mm+'/'+dd+'/'+yyyy;
+        var listElemText = "Journal " + today + ": " + jText;
+
+         $("#jList").prepend("<li class='collection-item'>" + listElemText + "</li>");
+    
+    });
+});
+
